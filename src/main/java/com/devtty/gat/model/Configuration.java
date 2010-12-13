24f42b23f831945393d9954a6113a00e3f8dabd8
Denis Renning <denis@devtty.de>
2010-12-13 16:05:15 +0100

    initial

---
+++ src/main/java/com/devtty/gat/model/Configuration.java
@@ -0,0 +1,37 @@
+package com.devtty.gat.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Configuration implements Serializable {
+	
+	private Long id;
+	private String name;
+	
+	@Id
+	@GeneratedValue
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	@NotNull
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
