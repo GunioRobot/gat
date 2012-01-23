3d46a277a6d82284459bcd1c32d494d54baed6b9
Denis Renning <drenning@intellibook-laptop.(none)>
2012-01-23 10:06:08 +0100

    Changes made by SparkleShare

---
+++ target/generated-sources/annotations/com/devtty/gat/model/Configuration_.java
@@ -0,0 +1,13 @@
+package com.devtty.gat.model;
+
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@StaticMetamodel(Configuration.class)
+public abstract class Configuration_ {
+
+	public static volatile SingularAttribute<Configuration, Long> id;
+	public static volatile SingularAttribute<Configuration, String> name;
+
+}
+
